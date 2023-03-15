6c18053350f53a768234772dcbc9606806158aa1
mrpisagor <koc.ataberk02@gmail.com>
2023-03-16 02:03:44 +0300

    new changes

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SimplePing
+SimplePing is written by python language. Using for send ping.
+```python
+
+sudo python3 ping.py HOST
+
+
+```
--- ping.py
+++ ping.py
@@ -8,6 +8,7 @@ from colorama import Fore, init
 import logging
 from os import urandom
 import sys
+
 init(autoreset=True)
 
 parser = ArgumentParser()
@@ -15,7 +16,7 @@ parser.add_argument("dest", help="destination ip address")
 parser.add_argument("-c", "--count", help="Packet count", type=int)
 args = parser.parse_args()
 
-logging.basicConfig(format="[+] %(level)s: %(message)s", level=logging.INFO)
+logging.basicConfig(format="[+] %(level)s: %(message)s", level=logging.DEBUG)
 logger = logging.getLogger("pinger")
 
 
@@ -33,32 +34,40 @@ def parse_response(res, ms):
     return f"Reply from {ip_layer.src} bytes={len(icmp_layer.load)} time={'{:.0f}'.format(ms)}ms ttl={ip_layer.ttl}"
 
 
-def ping_it(dest, **kwargs):
-    packet = scapy.IP(dst=dest, id=kwargs["r_id"])/scapy.ICMP(id=1,seq=kwargs["seq"])/kwargs["data"]
+def create_packet(dest,kwargs):
+    ip_packet = scapy.IP(dst=dest,id=kwargs["r_id"])
+    icmp_packet = scapy.ICMP(id=1,seq=kwargs["seq"])/kwargs["data"]
+    combined_packet = ip_packet/icmp_packet
+    return combined_packet
+
+def send_packet(dest, kwargs):
+    packet = create_packet(dest,kwargs)
+
     start = time.time()
     s, r = scapy.sr(packet, verbose=False, timeout=2)
     end = time.time()
     ms = (end-start)*1000
+    
     res = parse_response(s, ms)
-    print(res)
+    
+    logger.debug(Fore.GREEN+res,extra={"level": "Response"})
+
+
+def ping_target(dest,kwargs):
+    send_packet(dest,kwargs)
+    kwargs["seq"] += 1
+    sleep(1)
 
 
 @prepare
 def ping_loop(dest, count, **kwargs):
     try:
-        seq = 0
-        if count is not None:
-            for _ in range(count):
-                kwargs["seq"] = seq
-                ping_it(dest, **kwargs)
-                seq += 1
-                sleep(1)
-        else:
-            while True:
-                kwargs["seq"] = seq
-                ping_it(dest, **kwargs)
-                seq += 1
-                sleep(1)
+        kwargs["seq"] = 0 
+        
+        while count is None or kwargs["seq"] < count:
+
+            ping_target(dest,kwargs)
+    
     except KeyboardInterrupt:
         logger.info(Fore.CYAN+"exiting", extra={"level": "info"})
         exit(0)
@@ -66,7 +75,7 @@ def ping_loop(dest, count, **kwargs):
         logger.error(Fore.RED + "unknown address", extra={"level": "error"})
         exit(0)
     except Exception as e:
-        print(e)
+        logger.error(Fore.RED +str(e),{"level": "error"})
         exit(0)
 if __name__ == "__main__":
     ping_loop(args.dest, args.count)
